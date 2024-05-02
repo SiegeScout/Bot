9140a8ece25b8ac88a3ec9de74314ab8deee74ef
Max Moon <max@maxuk.me>
2024-05-02 23:54:23 +0100

    feat: handle command interactions

--- src/events/ChatInteraction.ts
+++ src/events/ChatInteraction.ts
@@ -1,6 +1,8 @@
 import type { ChatInputCommandInteraction } from "discord.js";
 import { commands } from "..";
 import Event from "@/structures/Event";
+import logger from "@/utils/logger";
+import { errorEmbed } from "@/utils/embeds";
 
 export default class ChatInteractionEvent extends Event {
   constructor() {
@@ -8,9 +10,25 @@ export default class ChatInteractionEvent extends Event {
   }
 
   async execute(interaction: ChatInputCommandInteraction) {
-    if (interaction.isChatInputCommand()) {
-      const command = commands.get(interaction.commandName);
-      if (command) command.execute(interaction);
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = commands.get(interaction.commandName);
+
+    if (!command) {
+      logger.error(`Unknown command ${interaction.commandName} executed by ${interaction.user.username}`);
+      return void await interaction.reply({
+        embeds: [errorEmbed('', '**Error!** There was an error executing this command')]
+      });
+    }
+
+    if (command.options?.guildOnly && !interaction.guild) return void await interaction.reply({
+      embeds: [errorEmbed('', '**Error!** This command can only be used in a server')]
+    });
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      logger.error(`Error executing command ${interaction.commandName} by ${interaction.user.username}`, error);
     }
   }
 }
