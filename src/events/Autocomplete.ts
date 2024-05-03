6a5c7d431b1258f2e2983a8803f61d10887a0b89
Max Moon <max@maxuk.me>
2024-05-03 13:22:39 +0100

    refactor: remove unneeded results slice

--- src/events/Autocomplete.ts
+++ src/events/Autocomplete.ts
@@ -16,11 +16,6 @@ export default class ChatInteractionEvent extends Event {
     const value = interaction.options.getFocused()
 
     const searchResults = await meilisearch.search('operators', value)
-    // if searcgresoluts exeeds 25 results, remove anything after 25
-    if (searchResults.hits.length > 25) {
-      searchResults.hits = searchResults.hits.slice(0, 25);
-    }
-
 
     console.log(searchResults.hits.map((hit) => hit['name'] + ' - ' + hit['slug']))
 
