8535b16dce1dce3c8cb557c05c29e0a9f9d35324
Max Moon <max@maxuk.me>
2024-05-10 00:51:15 +0100

    feat: Improve operator command with formatted embed and additional information

--- src/commands/Information/operator.ts
+++ src/commands/Information/operator.ts
@@ -2,36 +2,66 @@ import { MeiliSearchService } from "@/services/MeiliSearch";
 import SlashCommand from "@/structures/SlashCommand";
 import config from "@/utils/config";
 import { errorEmbed, primaryEmbed } from "@/utils/embeds";
-import { SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
+import {
+  SlashCommandBuilder,
+  type ChatInputCommandInteraction,
+} from "discord.js";
 
-const meili = new MeiliSearchService(config.MEILISEARCH_HOST, config.MEILISEARCH_KEY);
+const meili = new MeiliSearchService(
+  config.MEILISEARCH_HOST,
+  config.MEILISEARCH_KEY
+);
 
 export default class OperatorCommand extends SlashCommand {
   constructor() {
-    super('operator', 'Lookup information about a specific operator');
+    super("operator", "Lookup information about a specific operator");
   }
 
   async execute(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
 
-    const operator = interaction.options.getString('operator')
+    const operator = interaction.options.getString("operator");
 
     if (!operator) {
-      return void await interaction.editReply({
-        embeds: [errorEmbed('', '**Error!** You must provide an operator to look up.')],
-      });
+      return void (await interaction.editReply({
+        embeds: [
+          errorEmbed("", "**Error!** You must provide an operator to look up."),
+        ],
+      }));
     }
 
-    const operatorData = await meili.fetch('operators', operator)
+    const operatorData = await meili.fetch("operators", operator);
 
     if (!operatorData) {
-      return void await interaction.editReply({
-        embeds: [errorEmbed('', '**Error!** Unable to fetch operator from database, try again later.')],
-      });
+      return void (await interaction.editReply({
+        embeds: [
+          errorEmbed(
+            "",
+            "**Error!** Unable to fetch operator from database, try again later."
+          ),
+        ],
+      }));
     }
 
-    const embed = primaryEmbed('', `
-    **Name:** ${operatorData['name']}`).setAuthor({ name: operatorData['name'] })
+    const embed = primaryEmbed(operatorData["name"], "")
+      .addFields([
+        {
+          name: "General Information",
+          value:
+            `**Type:** ${operatorData["side"] == "defense" ? "Defender" : "Attacker"}\n` + `**Introduced:** Season ${operatorData["season"]}\n` +
+            `**Organisation:** tba\n` +
+            `**Squad:** tba\n`,
+          inline: true,
+        },
+        {
+          name: "_ _",
+          value: `**Gender:** tba\n` +
+            `**Country:** tba\n` +
+            `**Height:** tba\n` +
+            `**Weight:** tba\n`,
+          inline: true,
+        },
+      ]);
 
     await interaction.editReply({
       embeds: [embed],
@@ -42,10 +72,10 @@ export default class OperatorCommand extends SlashCommand {
     return new SlashCommandBuilder()
       .setName(this.name)
       .setDescription(this.description)
-      .addStringOption(option =>
+      .addStringOption((option) =>
         option
-          .setName('operator')
-          .setDescription('The operator to look up')
+          .setName("operator")
+          .setDescription("The operator to look up")
           .setRequired(true)
           .setAutocomplete(true)
       )
